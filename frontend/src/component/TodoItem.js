9ca0f4a007076fdae838af3fee136fd271e58a01
Akash-maurya1 <akashmaurya775395@gmail.com>
2024-07-07 23:51:46 -0500

    Add frontend directory

---
+++ frontend/src/component/TodoItem.js
@@ -0,0 +1,43 @@
+
+import React, { useState } from 'react';
+
+const TodoItem = ({ task, onDelete, onEdit }) => {
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTaskName, setNewTaskName] = useState(task.name);
+
+  const handleInputChange = (e) => {
+    setNewTaskName(e.target.value);
+  };
+
+  const handleEdit = () => {
+    onEdit(task.id, newTaskName);
+    setIsEditing(false);
+  };
+
+  return (
+    <li className="list-group-item d-flex justify-content-between align-items-center">
+      {isEditing ? (
+        <input  type="text"
+       
+          value={newTaskName}   onChange={handleInputChange}
+        />
+      ) : (
+        task.name
+      )}
+
+
+      <div>
+
+        {isEditing ? (
+          <button className="btn btn-success mx-2" onClick={handleEdit}>Save</button>
+        ) : (
+          <button className="btn btn-warning mx-2" onClick={() => setIsEditing(true)}>Edit</button>
+        )}
+        <button className="btn btn-danger" onClick={() => onDelete(task.id)}>Delete</button>
+      </div>
+    </li>
+  );
+};
+
+export default TodoItem;
---
+++ frontend/src/component/TodoList.js
@@ -0,0 +1,57 @@
+
+import React, { useState } from 'react';
+import TodoItem from './TodoItem';
+
+const TodoList = () => {
+
+  const [tasks, setTasks] = useState([]);
+  const [newTaskName, setNewTaskName] = useState('');
+
+  const handleInputChange = (e) => {
+    setNewTaskName(e.target.value);
+  };
+
+  const addTask = () => {
+    if (newTaskName !== '') {
+      const newTask = {  id: tasks.length + 1,
+        name: newTaskName,
+      };
+      setTasks([...tasks, newTask]);
+      setNewTaskName('');
+    }
+  };
+
+  const deleteTask = (taskId) => {
+
+    const updatedTasks = tasks.filter(task => task.id !== taskId);
+
+    setTasks(updatedTasks);
+  };
+
+  const editTask = (taskId, newName) => {
+    
+    const updatedTasks = tasks.map(task =>
+      task.id === taskId ? { ...task, name: newName } : task
+    );
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div className="container mt-5">
+      <h1 className="mb-4">Add Data</h1>
+      <div className="mb-3">
+        <input type="text"  placeholder="Enter Data "
+          value={newTaskName} onChange={handleInputChange}
+        />
+        <button className="btn btn-primary ml-2" onClick={addTask}>Add Task</button>
+      </div>
+      <ul className="list-group">
+        {tasks.map(task => (
+          <TodoItem key={task.id} task={task} onDelete={deleteTask} onEdit={editTask} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
